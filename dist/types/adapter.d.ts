63ae4a4fb5dc5d4b66a3fd1a598fe12a9c814c20
wangwenwen <48354328+wangwenwen130@users.noreply.github.com>
2026-07-02 14:02:35 +0800

    [feat] 移动端引入 openV3Mobile 放开 H5 硬解 (OPOOL-11465)

--- dist/types/adapter.d.ts
+++ dist/types/adapter.d.ts
@@ -1052,7 +1052,7 @@ declare class JSPlugin {
     player: EZopenPlayer;
     i18n: I18n.default;
     downloadRecord: any;
-    _recordPlugin: any;
+    _recordPlugin: PlayerPluginRecord;
     nWidth: number;
     nHeight: number;
     oStreamClient: StreamIO.default;
@@ -1102,8 +1102,8 @@ declare class JSPlugin {
      * @returns {none} 无
      */
     JS_Disable3DZoom(): none;
-    JS_StartSave(fileName: string | undefined, stopCallback: any, secretKey: any): any;
-    JS_StopSave(download?: boolean): any;
+    JS_StartSave(fileName: string | undefined, stopCallback: any, secretKey: any): Promise<void>;
+    JS_StopSave(download?: boolean): Promise<unknown>;
     _JSPlayM4_GetFrameInfo(): IFrameInfo;
     _JSPlayM4_SetDisplayRegion(left: any, right: any, top: any, bottom: any, flag: boolean | undefined, isFullScreen: any): boolean;
     JS_CapturePicture(port: any, fileName: any, format: any, callback: any, download: any, canvas: any): Promise<any>;
